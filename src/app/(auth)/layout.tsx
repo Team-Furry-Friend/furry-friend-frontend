df8984c5aba2a9da96c45b6b87a9398f42538be4
kkukileon305 <kkukileon305@gmail.com>
2023-06-14 01:52:21 +0900

    add auth layout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,7 +1,28 @@
 import { ReactNode } from 'react';
+import Link from 'next/link';
+import { Bevan } from 'next/font/google';
+
+const font = Bevan({
+  weight: '400',
+  subsets: ['latin'],
+});
 
 const Layout = ({ children }: { children: ReactNode }) => {
-  return <div>{children}</div>;
+  return (
+    <div className='min-h-[100dvh] flex justify-center items-center p-2 md:p-4'>
+      <div className='max-w-md w-full flex flex-col gap-4 border p-2 md:p-4 rounded shadow'>
+        <h1 className={`${font.className} text-xl text-center`}>
+          Furry Friend
+        </h1>
+
+        {children}
+
+        <Link href={'/'} className='text-center'>
+          메인으로 돌아가기
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Layout;
