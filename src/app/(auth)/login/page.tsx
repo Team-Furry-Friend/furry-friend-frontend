47d22dbbc7149082e993b805072c52186a7d944d
kkukileon305 <kkukileon305@ofotd.com>
2023-06-13 11:20:11 +0900

    initialize projects

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Login',
+};
+
+const Page = () => {
+  return <div>Login</div>;
+};
+
+export default Page;
---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Register',
+};
+
+const Page = () => {
+  return <div>Register</div>;
+};
+
+export default Page;
