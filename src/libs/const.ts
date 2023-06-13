09491d1ee23b12d1362f86890fd4ab17b7414de9
kkukileon305 <kkukileon305@gmail.com>
2023-06-14 00:59:50 +0900

    add authMenus data

---
+++ src/libs/const.ts
@@ -0,0 +1,15 @@
+type Menu = {
+  title: string;
+  href: string;
+};
+
+export const authMenus: Menu[] = [
+  {
+    title: '로그인',
+    href: '/login',
+  },
+  {
+    title: '회원가입',
+    href: '/register',
+  },
+];
