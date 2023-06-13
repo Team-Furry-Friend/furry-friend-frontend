365f9be84e21ab14ffd42c8e9819f57d6dd7c4b4
kkukileon305 <kkukileon305@gmail.com>
2023-06-14 01:41:27 +0900

    add todo comment

--- src/components/lists/AuthMenus.tsx
+++ src/components/lists/AuthMenus.tsx
@@ -1,9 +1,12 @@
 import { cookies } from 'next/headers';
-import { authMenus } from '@/libs/const';
 import Link from 'next/link';
+import { authMenus } from '@/libs/const';
+import LogoutBtn from '@/components/buttons/LogoutBtn';
 
 const AuthMenus = async () => {
   const cookieStore = cookies();
+
+  // TODO: 쿠키와 토큰 검증 API를 이용해 로그인 여부를 받아와서 ui 변경 로직 추가
 
   return (
     <ul className='flex gap-4'>
