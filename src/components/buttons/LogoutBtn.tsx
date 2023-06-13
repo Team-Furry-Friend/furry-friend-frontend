abd4a910762605fc74a3476b34b575ed07c2ea75
kkukileon305 <kkukileon305@gmail.com>
2023-06-14 01:41:43 +0900

    add LogoutBtn and todo comment

---
+++ src/components/buttons/LogoutBtn.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+const LogoutBtn = () => {
+  const onClick = async () => {
+    // TODO: 로그아웃 로직
+  };
+
+  return <button onClick={onClick}>로그아웃</button>;
+};
+
+export default LogoutBtn;
