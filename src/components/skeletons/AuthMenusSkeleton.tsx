4506f36e2cb61bd825f0dd4a657081ed47f6ddd7
kkukileon305 <kkukileon305@gmail.com>
2023-06-14 01:21:06 +0900

    add pulse animation in skeleton component

--- src/components/skeletons/AuthMenusSkeleton.tsx
+++ src/components/skeletons/AuthMenusSkeleton.tsx
@@ -1,8 +1,8 @@
 const AuthMenusSkeleton = () => {
   return (
     <div className='flex gap-4'>
-      <div className='w-12 h-6 bg-gray-200 rounded' />
-      <div className='w-16 h-6 bg-gray-200 rounded' />
+      <div className='w-12 h-6 bg-gray-200 rounded animate-pulse' />
+      <div className='w-16 h-6 bg-gray-200 rounded animate-pulse' />
     </div>
   );
 };
