08d93ebb164a6970f361cee762700e7abfe55048
kkukileon305 <kkukileon305@gmail.com>
2023-06-14 18:47:39 +0900

    fix footer layout

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -11,22 +11,28 @@ const Footer = () => {
   return (
     <footer className='bg-gray-100'>
       <div className='max-w-5xl mx-auto p-2 md:p-4 flex flex-col gap-4'>
-        <div className={`flex gap-4 items-center ${font.className}`}>
-          <Image
-            src='/icons/chinchilla.png'
-            alt='logo'
-            width={50}
-            height={50}
-            className='w-6'
-          />
-          <p className='block'>Furry Friend</p>
-        </div>
-
         <div className='flex gap-4 items-center'>
+          <div className={`flex gap-4 items-center ${font.className}`}>
+            <Image
+              src='/icons/chinchilla.png'
+              alt='logo'
+              width={50}
+              height={50}
+              className='w-6'
+            />
+            <p className='block'>Furry Friend</p>
+          </div>
+          <div className='w-[1px] h-4 bg-gray-400' />
           <Link href={'/terms-of-service'}>이용약관</Link>
           <div className='w-[1px] h-4 bg-gray-400' />
           <Link href={'/privacy'}>개인정보 처리방침</Link>
         </div>
+        <Link
+          href={'https://github.com/kkukileon305/furry-friend'}
+          target='_blank'
+        >
+          GitHub
+        </Link>
       </div>
     </footer>
   );
