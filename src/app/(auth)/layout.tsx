200001743f3a88f25ab995d0c29f6e1c497286f6
kkukileon305 <kkukileon305@gmail.com>
2023-06-14 13:24:01 +0900

    change auth page layout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -10,27 +10,32 @@ const font = Bevan({
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className='min-h-[100dvh] flex justify-center items-center p-2 md:p-4'>
-      <div className='max-w-md w-full flex flex-col gap-4 border p-2 md:p-4 rounded shadow'>
-        <h1
-          className={`${font.className} text-xl flex justify-center items-center gap-4`}
-        >
-          <Image
-            src='/icons/chinchilla.png'
-            alt='logo'
-            width={50}
-            height={50}
-            className='w-8'
-          />
+    <div className='flex'>
+      <div className='md:w-1/2 bg-gray-100 hidden lg:flex justify-center items-center'>
+        <h2 className='text-3xl'>Furry Friend에 오신 것을 환영해요!</h2>
+      </div>
+      <div className='w-full lg:w-1/2 min-h-[100dvh] flex justify-center items-center p-2 md:p-4'>
+        <div className='max-w-md w-full flex flex-col gap-4 p-2 md:p-4'>
+          <h1
+            className={`${font.className} text-xl flex justify-center items-center gap-4`}
+          >
+            <Image
+              src='/icons/chinchilla.png'
+              alt='logo'
+              width={50}
+              height={50}
+              className='w-8'
+            />
 
-          <p>Furry Friend</p>
-        </h1>
+            <p>Furry Friend</p>
+          </h1>
 
-        {children}
+          {children}
 
-        <Link href={'/'} className='text-center'>
-          메인으로 돌아가기
-        </Link>
+          <Link href={'/'} className='text-center'>
+            메인으로 돌아가기
+          </Link>
+        </div>
       </div>
     </div>
   );
