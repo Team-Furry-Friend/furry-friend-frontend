3e0b873e49b17f2392065e064ed911cd15cde81c
kkukileon305 <kkukileon305@gmail.com>
2023-06-15 15:34:40 +0900

    fix NoticeModal: add multiple texts

--- src/components/modals/NoticeModal.tsx
+++ src/components/modals/NoticeModal.tsx
@@ -1,16 +1,20 @@
 'use client';
 
 interface NoticeModal {
-  text: string;
+  texts: string[];
   onClose?: () => void;
 }
 
-const NoticeModal = ({ onClose, text }: NoticeModal) => {
+const NoticeModal = ({ onClose, texts }: NoticeModal) => {
   return (
     <div className='flex flex-col'>
       <div className='p-2 md:p-4 flex flex-col gap-4 items-center'>
         <h2 className='font-bold text-xl'>알림</h2>
-        <p>{text}</p>
+        <ul className='flex flex-col gap-2 text-center'>
+          {texts.map(text => (
+            <li key={text}>{text}</li>
+          ))}
+        </ul>
       </div>
       <button
         className='bg-blue-400 py-2 text-white font-bold'
