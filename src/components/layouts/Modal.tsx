2118d79d07fae92aef940bf83d8b1cdebdb70937
kkukileon305 <kkukileon305@gmail.com>
2023-06-15 15:05:46 +0900

    add modal layout and close event

--- src/components/layouts/Modal.tsx
+++ src/components/layouts/Modal.tsx
@@ -1,11 +1,29 @@
 'use client';
 
 import { useModal } from '@/store/modalStore';
+import { MouseEventHandler } from 'react';
 
 const Modal = () => {
   const { modal, setModal } = useModal();
 
-  return <div className={`${modal ? 'fixed' : 'hidden'}`}>Modal</div>;
+  const handleClose: MouseEventHandler<HTMLElement> = e => {
+    if (!(e.target instanceof Element)) return;
+
+    if (e.target === e.currentTarget || e.target.id === 'close') {
+      setModal(null);
+    }
+  };
+
+  return (
+    <div
+      className={`${
+        modal ? 'fixed' : 'hidden'
+      } z-10 bg-black/50 w-full h-full flex justify-center items-center p-2 md:p-4`}
+      onClick={handleClose}
+    >
+      <div className='bg-white rounded max-w-xl w-full'>{modal}</div>
+    </div>
+  );
 };
 
 export default Modal;
