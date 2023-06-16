9218e0b0ed15712d8833774cdfe589a271fcb3d5
kkukileon305 <kkukileon305@gmail.com>
2023-06-17 01:06:36 +0900

    create error page

---
+++ src/app/error.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+const Error = () => {
+  return (
+    <div className='min-h-[100dvh] flex flex-col items-center justify-center'>
+      <h1 className='font-bold text-4xl'>404</h1>
+      <p className='font-bold text-blue-400'>
+        알 수 없는 에러가 발생하였습니다.
+      </p>
+    </div>
+  );
+};
+
+export default Error;
