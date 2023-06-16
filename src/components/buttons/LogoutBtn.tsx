ea40d3763ca7119d4c163e06ff092603578784b0
kkukileon305 <kkukileon305@gmail.com>
2023-06-16 23:07:18 +0900

    add disable stats in logout button

--- src/components/buttons/LogoutBtn.tsx
+++ src/components/buttons/LogoutBtn.tsx
@@ -3,11 +3,16 @@
 import { useRouter } from 'next/navigation';
 import { api } from '@/libs/api';
 import Cookies from 'js-cookie';
+import { useState } from 'react';
 
 const LogoutBtn = ({ at }: { at: string }) => {
   const router = useRouter();
 
+  const [isLoading, setIsLoading] = useState(false);
+
   const onClick = async () => {
+    setIsLoading(true);
+
     try {
       await api.post(`/member/logout`, {
         access_token: at,
@@ -15,6 +20,8 @@ const LogoutBtn = ({ at }: { at: string }) => {
     } catch (e) {
       console.log(e);
     }
+
+    setIsLoading(false);
 
     Cookies.set('access_token', '', {
       expires: 0,
@@ -24,7 +31,15 @@ const LogoutBtn = ({ at }: { at: string }) => {
     router.push('/login');
   };
 
-  return <button onClick={onClick}>로그아웃</button>;
+  return (
+    <button
+      disabled={isLoading}
+      onClick={onClick}
+      className='bg-blue-400 hover:bg-blue-200 disabled:bg-gray-200 rounded px-2 py-1 text-white font-bold'
+    >
+      로그아웃
+    </button>
+  );
 };
 
 export default LogoutBtn;
