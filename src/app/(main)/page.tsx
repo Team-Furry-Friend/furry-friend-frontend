38d3de95a53e0fd20234070bd70603ad8f49a871
kkukileon305 <kkukileon305@gmail.com>
2023-06-17 01:09:36 +0900

    create upload button

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,10 +1,18 @@
+import UploadBtn from '@/components/buttons/UploadBtn';
+
 const Page = () => {
   return (
-    <div>
+    <>
       <div className='h-80 bg-fuchsia-400 flex justify-center items-center text-white font-bold text-3xl'>
         <h2>Furry Friend</h2>
       </div>
-    </div>
+
+      <div className='max-w-6xl mx-auto p-2 md:p-4 flex flex-col items-center gap-8'>
+        <h2 className='w-full font-bold text-2xl'>최근 등록된 상품들</h2>
+      </div>
+
+      <UploadBtn />
+    </>
   );
 };
 
---
+++ src/components/buttons/UploadBtn.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+const UploadBtn = () => {
+  return (
+    <button className='fixed right-4 bottom-4 bg-blue-400 aspect-square rounded-full overflow-hidden p-4 font-bold text-white'>
+      업로드
+    </button>
+  );
+};
+
+export default UploadBtn;
