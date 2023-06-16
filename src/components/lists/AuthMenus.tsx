24577fca08e78492517abb3d894c7cef680ff488
kkukileon305 <kkukileon305@gmail.com>
2023-06-16 18:38:54 +0900

    add authenticated ui

--- src/components/lists/AuthMenus.tsx
+++ src/components/lists/AuthMenus.tsx
@@ -2,11 +2,25 @@ import { cookies } from 'next/headers';
 import Link from 'next/link';
 import { authMenus } from '@/datas/menuData';
 import LogoutBtn from '@/components/buttons/LogoutBtn';
+import { NextResponse } from 'next/server';
 
 const AuthMenus = async () => {
   const cookieStore = cookies();
+  const at = cookieStore.get('access_token')?.value;
 
-  // TODO: 쿠키와 토큰 검증 API를 이용해 로그인 여부를 받아와서 ui 변경 로직 추가
+  if (at) {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/gateway/isvalid/${at}`
+    );
+
+    const body = await response.json();
+
+    if (body.status === 'success') {
+      console.log('at - authmenus', body.status);
+
+      return <LogoutBtn at={at} />;
+    }
+  }
 
   return (
     <ul className='flex gap-4'>
