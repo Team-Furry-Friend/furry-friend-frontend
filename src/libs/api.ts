97f247f28de303e6070517fe58641e21881ecdf0
kkukileon305 <kkukileon305@gmail.com>
2023-06-17 20:18:27 +0900

    add supabase module for storage

--- src/libs/api.ts
+++ src/libs/api.ts
@@ -1,6 +1,11 @@
 import axios from 'axios';
+import { createClient } from '@supabase/supabase-js';
 
 export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
   withCredentials: true,
 });
+
+const supabaseUrl = 'https://pnfgdtoqziegilxdoirt.supabase.co';
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY as string;
+export const supabase = createClient(supabaseUrl, supabaseKey);
