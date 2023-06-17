e32b071bee9d481b41bb14aabd29c7b4d68cea65
kkukileon305 <kkukileon305@gmail.com>
2023-06-17 20:32:37 +0900

    split upload layout and need auth layout

--- src/app/(main)/upload/page.tsx
+++ src/app/(main)/upload/page.tsx
@@ -1,30 +1,27 @@
 import { cookies } from 'next/headers';
 import Link from 'next/link';
+import UploadForm from '@/app/(main)/upload/UploadForm';
+import Auth from '@/components/layouts/Auth';
 
 const Page = async () => {
   const cookieStore = cookies();
   const at = cookieStore.get('access_token')?.value;
 
   if (!at) {
-    return (
-      <div className='min-h-[calc(100dvh-48px)] md:min-h-[calc(100dvh-64px)] max-w-6xl mx-auto p-2 md:p-4 flex justify-center items-center'>
-        <div className='max-w-2xl w-full border rounded flex flex-col items-center gap-4 p-4'>
-          <p className='font-bold text-xl text-center'>
-            지금 가입하고 <br /> 당신의 상품을 업로드 하세요!
-          </p>
-
-          <Link
-            href={'/login'}
-            className='w-fit p-2 rounded font-bold text-white bg-blue-400'
-          >
-            로그인하기
-          </Link>
-        </div>
-      </div>
-    );
+    return <Auth />;
   }
 
-  return <div className='max-w-6xl mx-auto p-2 md:p-4'>Page</div>;
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}/gateway/isvalid/${at}`
+  );
+
+  const isValid = (await response.json()).status === 'success';
+
+  if (!isValid) {
+    return <Auth />;
+  }
+
+  return <UploadForm />;
 };
 
 export default Page;
