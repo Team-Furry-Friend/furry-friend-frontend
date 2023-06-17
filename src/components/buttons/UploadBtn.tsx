867597fda0afc43841a1fba03a41092b7390b91c
kkukileon305 <kkukileon305@gmail.com>
2023-06-17 19:48:22 +0900

    change upload button styling

--- src/components/buttons/UploadBtn.tsx
+++ src/components/buttons/UploadBtn.tsx
@@ -1,10 +1,15 @@
 'use client';
 
+import Link from 'next/link';
+
 const UploadBtn = () => {
   return (
-    <button className='fixed right-4 bottom-4 bg-blue-400 aspect-square rounded-full overflow-hidden p-4 font-bold text-white'>
+    <Link
+      href={'/upload'}
+      className='flex justify-center items-center fixed left-1/2 -translate-x-1/2 bottom-4 bg-blue-400 hover:bg-blue-200 rounded-full p-2 font-bold text-white'
+    >
       업로드
-    </button>
+    </Link>
   );
 };
 
