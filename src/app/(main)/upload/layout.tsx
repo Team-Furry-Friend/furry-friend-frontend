f05aa681b23b55a02733c71e85c452ac687870ec
kkukileon305 <kkukileon305@gmail.com>
2023-06-17 20:50:22 +0900

    change auth layout

--- src/app/(main)/upload/layout.tsx
+++ src/app/(main)/upload/layout.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react';
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className='min-h-[calc(100dvh-48px)] md:min-h-[calc(100dvh-64px)] max-w-6xl mx-auto p-2 md:p-4 flex justify-center items-center'>
+    <div className='max-w-6xl mx-auto p-2 md:p-4 flex justify-center items-center'>
       {children}
     </div>
   );
