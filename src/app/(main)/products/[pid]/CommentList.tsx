99748adbf89b17da5654263bbff08ee84aa3c79b
kkukileon305 <kkukileon305@gmail.com>
2023-06-26 15:13:23 +0900

    create comment list component

---
+++ src/app/(main)/products/[pid]/CommentList.tsx
@@ -0,0 +1,21 @@
+import { Comment } from '@/types';
+import { getDateDiff } from '@/libs/getDateDiff';
+
+const CommentList = ({ comments }: { comments: Comment[] }) => {
+  return (
+    <ul className='mb-32'>
+      {comments.reverse().map(comment => (
+        <li key={comment.rid} className='py-8 flex flex-col gap-4 border-b'>
+          <div className='flex gap-2'>
+            <p className='font-bold'>{comment.nickname || '닉넴없음'}</p>
+            <p>{getDateDiff(comment.regDate)}</p>
+          </div>
+
+          <p className='whitespace-pre'>{comment.text}</p>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default CommentList;
