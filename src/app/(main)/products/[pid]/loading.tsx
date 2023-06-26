f9fa31f6bf6ac3b740be1bba6445b045812c4d0b
kkukileon305 <kkukileon305@gmail.com>
2023-06-26 15:23:17 +0900

    add comment skeleton ui

--- src/app/(main)/products/[pid]/loading.tsx
+++ src/app/(main)/products/[pid]/loading.tsx
@@ -2,7 +2,7 @@ const Loading = () => {
   return (
     <div className='w-full flex flex-col gap-4'>
       <div className='w-8 h-8 bg-gray-200 rounded-full animate-pulse mb-4' />
-      <div className='w-24 h-4 bg-gray-200 rounded animate-pulse' />
+      <div className='w-24 h-11 bg-gray-200 rounded animate-pulse' />
       <div className='w-24 h-4 bg-gray-200 rounded animate-pulse' />
       <div className='w-36 h-6 bg-gray-200 rounded animate-pulse' />
       <div className='w-24 h-4 bg-gray-200 rounded animate-pulse' />
@@ -14,6 +14,22 @@ const Loading = () => {
         <div className='w-[calc(50%-4px)] md:w-[calc((100%-48px)/4)] h-64 bg-gray-200 rounded animate-pulse' />
       </div>
       <div className='w-24 h-4 bg-gray-200 rounded animate-pulse' />
+      <div className='w-32 h-4 bg-gray-200 rounded animate-pulse' />
+      <div className='w-16 h-4 bg-gray-200 rounded animate-pulse' />
+
+      <div className='h-[1px] bg-gray-200 my-4' />
+
+      <div className='bg-gray-200 animate-pulse h-32 rounded mb-8' />
+
+      <div className='flex flex-col border-b pb-8 mb-32'>
+        <div className='flex gap-4'>
+          <div className='w-16 h-6 bg-gray-200 rounded animate-pulse' />
+          <div className='w-16 h-6 bg-gray-200 rounded animate-pulse' />
+        </div>
+
+        <div className='w-32 h-4 bg-gray-200 rounded animate-pulse mt-4' />
+        <div className='w-16 h-4 bg-gray-200 rounded animate-pulse mt-4' />
+      </div>
     </div>
   );
 };
