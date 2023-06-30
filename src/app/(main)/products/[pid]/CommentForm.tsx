dc00ae0b0b753f0fe58d364168b2dfa06085aa45
kkukileon305 <kkukileon305@gmail.com>
2023-06-30 22:58:40 +0900

    add comment skeleton when comment uploading

--- src/app/(main)/products/[pid]/CommentForm.tsx
+++ src/app/(main)/products/[pid]/CommentForm.tsx
@@ -52,33 +52,46 @@ const CommentForm = ({ at, pid }: CommentFormProps) => {
   };
 
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className='flex flex-col gap-4 py-2 rounded'
-    >
-      <textarea
-        {...register('text', {
-          required: '댓글을 작성해주세요.',
-          minLength: {
-            value: 4,
-            message: '최소 4글자 이상 작성해주세요.',
-          },
-        })}
-        className='w-full h-32 rounded border p-2 resize-none focus:outline-blue-500'
-        placeholder='댓글 입력하기...'
-      />
+    <>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className='flex flex-col gap-4 py-2 rounded'
+      >
+        <textarea
+          {...register('text', {
+            required: '댓글을 작성해주세요.',
+            minLength: {
+              value: 4,
+              message: '최소 4글자 이상 작성해주세요.',
+            },
+          })}
+          className='w-full h-32 rounded border p-2 resize-none focus:outline-blue-500'
+          placeholder='댓글 입력하기...'
+        />
 
-      <div className='flex flex-row-reverse justify-between items-center'>
-        <button
-          type='submit'
-          className='bg-blue-400 hover:bg-blue-200 disabled:bg-gray-200 font-bold text-white rounded p-2'
-          disabled={isLoading}
-        >
-          작성하기
-        </button>
-        {errors.text && <p className='text-red-400'>{errors.text.message}</p>}
-      </div>
-    </form>
+        <div className='flex flex-row-reverse justify-between items-center'>
+          <button
+            type='submit'
+            className='bg-blue-400 hover:bg-blue-200 disabled:bg-gray-200 font-bold text-white rounded p-2'
+            disabled={isLoading}
+          >
+            작성하기
+          </button>
+          {errors.text && <p className='text-red-400'>{errors.text.message}</p>}
+        </div>
+      </form>
+
+      {isLoading && (
+        <div className='flex flex-col gap-4 mt-8'>
+          <div className='flex gap-4'>
+            <div className='h-6 w-8 bg-gray-200 rounded animate-pulse' />
+            <div className='h-6 w-8 bg-gray-200 rounded animate-pulse' />
+          </div>
+          <div className='h-6 w-1/2 bg-gray-200 rounded animate-pulse' />
+          <div className='h-6 w-1/3 bg-gray-200 rounded animate-pulse' />
+        </div>
+      )}
+    </>
   );
 };
 
