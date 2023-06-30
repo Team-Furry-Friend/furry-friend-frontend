0f6a39fa9eeeb35872d064071d23e28ecd7ab99f
kkukileon305 <kkukileon305@gmail.com>
2023-06-30 23:05:26 +0900

    add banner navigation skeleton

--- src/components/skeletons/BannerSkeleton.tsx
+++ src/components/skeletons/BannerSkeleton.tsx
@@ -1,6 +1,16 @@
 const BannerSkeleton = () => {
   return (
-    <div className='bg-gray-200 h-[320px] md:h-[384px] animate-pulse'></div>
+    <>
+      <div className='bg-gray-200 h-[320px] md:h-[384px] animate-pulse' />
+      <div className='center'>
+        <ul className='flex gap-4 mt-4'>
+          <li className='w-24 h-[34px] bg-gray-200 rounded-full animate-pulse' />
+          <li className='w-20 h-[34px] bg-gray-200 rounded-full animate-pulse' />
+          <li className='w-16 h-[34px] bg-gray-200 rounded-full animate-pulse' />
+          <li className='w-20 h-[34px] bg-gray-200 rounded-full animate-pulse' />
+        </ul>
+      </div>
+    </>
   );
 };
 
