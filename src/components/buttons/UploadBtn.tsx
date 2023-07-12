bb1f4ebff13c9c00954eb73bb820a320574fa68d
kkukileon305 <kkukileon305@gmail.com>
2023-07-12 15:40:55 +0900

    add shadow in upload btn

--- src/components/buttons/UploadBtn.tsx
+++ src/components/buttons/UploadBtn.tsx
@@ -6,7 +6,7 @@ const UploadBtn = () => {
   return (
     <Link
       href={'/upload'}
-      className='flex justify-center items-center fixed left-1/2 -translate-x-1/2 bottom-4 bg-blue-400 hover:bg-blue-200 rounded-full p-2 font-bold text-white'
+      className='flex justify-center items-center fixed left-1/2 -translate-x-1/2 bottom-4 bg-blue-400 hover:bg-blue-200 shadow-xl rounded-full p-2 font-bold text-white'
     >
       상품 올리기
     </Link>
