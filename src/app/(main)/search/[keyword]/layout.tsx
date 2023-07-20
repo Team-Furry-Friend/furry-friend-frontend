8d839cbfb1c97bebdca251a9e91d8123f7109e72
kkukileon305 <kkukileon305@gmail.com>
2023-07-21 02:22:03 +0900

    split layout and page in category and search page

---
+++ src/app/(main)/search/[keyword]/layout.tsx
@@ -0,0 +1,28 @@
+import { ReactNode } from 'react';
+import CategoryList from '@/components/lists/CategoryList';
+import SearchBtn from '@/components/buttons/SearchBtn';
+
+const Layout = ({
+  params,
+  children,
+}: {
+  params: { keyword: string };
+  children: ReactNode;
+}) => {
+  return (
+    <div className='w-full'>
+      <div className='h-8 flex justify-between items-center my-4'>
+        <h2 className='w-full font-bold text-2xl'>
+          {decodeURIComponent(params.keyword)}
+        </h2>
+
+        <CategoryList />
+        <SearchBtn />
+      </div>
+
+      {children}
+    </div>
+  );
+};
+
+export default Layout;
