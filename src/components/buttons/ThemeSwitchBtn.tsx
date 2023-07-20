e98d3c0851e37399b2f22b4dc390913a80a6abf5
kkukileon305 <kkukileon305@gmail.com>
2023-07-21 00:15:09 +0900

    create theme switch button

--- src/components/buttons/ThemeSwitchBtn.tsx
+++ src/components/buttons/ThemeSwitchBtn.tsx
@@ -5,10 +5,9 @@ import Cookies from 'js-cookie';
 
 const ThemeSwitchBtn = () => {
   const router = useRouter();
+  const theme = Cookies.get('theme');
 
   const onClick = () => {
-    const theme = Cookies.get('theme');
-
     Cookies.set('theme', theme === 'dark' ? 'light' : 'dark', {
       expires: 365,
     });
@@ -16,7 +15,20 @@ const ThemeSwitchBtn = () => {
     router.refresh();
   };
 
-  return <button onClick={onClick}>ThemeSwitchBtn</button>;
+  return (
+    <button
+      onClick={onClick}
+      className={`transition-all border rounded-full w-14 h-8 px-1 ${
+        theme === 'dark' ? 'bg-gray-400' : 'bg-gray-600'
+      }`}
+    >
+      <div
+        className={`h-6 transition-transform aspect-square rounded-full bg-white ${
+          theme === 'dark' ? 'translate-x-6' : 'translate-x-0'
+        }`}
+      />
+    </button>
+  );
 };
 
 export default ThemeSwitchBtn;
