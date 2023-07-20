6712318a93336b069a9ea6b20384639db37a02de
kkukileon305 <kkukileon305@gmail.com>
2023-07-20 15:56:17 +0900

    add icon in category list

--- src/components/lists/CategoryList.tsx
+++ src/components/lists/CategoryList.tsx
@@ -1,12 +1,14 @@
 import Link from 'next/link';
+import { BiCategory } from 'react-icons/bi';
 
 const categories = ['사료', '간식', '용품', '의류'];
 
 const CategoryList = () => {
   return (
-    <div className='group/category relative h-full w-20 md:w-24'>
-      <button className='h-full px-2 md:px-4 bg-white group-hover/category:bg-gray-200'>
-        카테고리
+    <div className='group/category relative h-full w-16 md:w-32'>
+      <button className='flex justify-center items-center gap-2 w-full h-full px-2 md:px-4 bg-white group-hover/category:bg-gray-200'>
+        <BiCategory size={20} />
+        <p className='hidden md:block'>카테고리</p>
       </button>
 
       <ul className='absolute w-full hidden group-hover/category:block bg-white shadow rounded-b-lg overflow-hidden'>
