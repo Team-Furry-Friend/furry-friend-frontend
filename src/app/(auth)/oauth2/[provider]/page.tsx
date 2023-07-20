2c9397f1e32abd9090642058b315b5e42e934258
kkukileon305 <kkukileon305@gmail.com>
2023-07-20 14:32:13 +0900

    create oauth page

---
+++ src/app/(auth)/oauth2/[provider]/page.tsx
@@ -0,0 +1,11 @@
+import { ImSpinner8 } from 'react-icons/im';
+
+const Page = () => {
+  return (
+    <div className='flex justify-center items-center h-32'>
+      <ImSpinner8 className='animate-spin text-gray-200' size={32} />
+    </div>
+  );
+};
+
+export default Page;
