f9979d969af602c0d17cf5ba8667b80d085b955f
kkukileon305 <kkukileon305@gmail.com>
2023-08-07 15:03:50 +0900

    add input(상품 내용) size height in upload form

--- src/app/(main)/upload/loading.tsx
+++ src/app/(main)/upload/loading.tsx
@@ -14,8 +14,8 @@ const Loading = () => {
           <div className='w-full h-11 bg-gray-200 rounded animate-pulse' />
         </div>
         <div className='flex flex-col gap-2'>
-          <div className='w-24 h-40 bg-gray-200 rounded animate-pulse' />
-          <div className='w-full h-11 bg-gray-200 rounded animate-pulse' />
+          <div className='w-24 h-6 bg-gray-200 rounded animate-pulse' />
+          <div className='w-full h-40 bg-gray-200 rounded animate-pulse' />
         </div>
 
         <div className='h-10 bg-gray-200 rounded animate-pulse' />
