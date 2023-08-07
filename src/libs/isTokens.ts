5b21aa1a5013caa36eb747b98f9764c6a6e5c251
kkukileon305 <kkukileon305@gmail.com>
2023-08-08 02:53:02 +0900

    remove unused function

--- src/libs/isTokens.ts
+++
@@ -1,8 +0,0 @@
-import { Tokens, User } from '@/types';
-
-export function isTokens(data: Tokens | User): data is Tokens {
-  return (
-    (data as Tokens).accessToken !== undefined &&
-    (data as Tokens).refreshToken !== undefined
-  );
-}
