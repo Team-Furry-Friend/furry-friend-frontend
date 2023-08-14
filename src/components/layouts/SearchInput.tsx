0c6362a270f682aa10d78b1b3eed85d751ee95a6
kkukileon305 <kkukileon305@gmail.com>
2023-08-14 15:19:09 +0900

    add onSubmit handler

--- src/components/layouts/SearchInput.tsx
+++ src/components/layouts/SearchInput.tsx
@@ -1,15 +1,18 @@
 'use client';
 
-import { ChangeEventHandler, useRef, useState } from 'react';
+import { ChangeEventHandler, FormEventHandler, useRef, useState } from 'react';
 import { BsSearch } from 'react-icons/bs';
 import AutoSearchList from '@/app/(main)/AutoSearchList';
 import { products } from '@/libs/api';
 import { Product } from '@/types';
+import { useRouter } from 'next/navigation';
 
 const SearchInput = () => {
+  const router = useRouter();
+
   const [keyword, setKeyword] = useState('');
-  const debounce = useRef<ReturnType<typeof setTimeout>>();
   const [productList, setProductList] = useState<Product[]>([]);
+  const debounce = useRef<ReturnType<typeof setTimeout>>();
 
   const onChange: ChangeEventHandler<HTMLInputElement> = e => {
     setKeyword(e.target.value);
@@ -28,17 +31,30 @@ const SearchInput = () => {
     }, 500);
   };
 
+  const onSubmit: FormEventHandler = e => {
+    e.preventDefault();
+
+    if (keyword.length > 0) {
+      router.push(`/search/${keyword}`);
+    }
+  };
+
   return (
     <div className='hidden md:block group'>
-      <div className='p-2 rounded bg-gray-200 dark:bg-gray-600 w-60 flex items-center'>
-        <BsSearch />
+      <form
+        onSubmit={onSubmit}
+        className='p-2 rounded bg-gray-200 dark:bg-gray-600 w-60 flex items-center'
+      >
+        <button>
+          <BsSearch />
+        </button>
         <input
           type='text'
           className='block w-full pl-2'
           placeholder='어떤 상품을 찾으시나요?'
           onChange={onChange}
         />
-      </div>
+      </form>
 
       <div
         tabIndex={0}
