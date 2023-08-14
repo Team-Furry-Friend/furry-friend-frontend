3dbc283a621c50fdde9e19ad8f800fc581e70e3c
kkukileon305 <kkukileon305@gmail.com>
2023-08-14 14:42:35 +0900

    add breakpoint for search input

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        search: '940px',
+      },
+    },
   },
   plugins: [],
   darkMode: 'class',
